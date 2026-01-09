479d56de3473c7374f5aebaadaaa14112b1b8396
h.kobayashi <kobayashih376@gmail.com>
2026-01-09 20:51:08 +0900

    fix(ci): fix LHCI assertions & add robots.txt for SEO

--- frontend/lighthouserc.cjs
+++ frontend/lighthouserc.cjs
@@ -18,12 +18,12 @@ module.exports = {
       },
     },
     assert: {
-      preset: 'lighthouse:no-pwa',
       assertions: {
         'categories:performance': ['error', { minScore: 0.75 }],
         'categories:accessibility': ['error', { minScore: 0.9 }],
         'categories:best-practices': ['error', { minScore: 0.9 }],
         'categories:seo': ['error', { minScore: 0.9 }],
+        'categories:pwa': 'off',
       },
     },
     upload: {
